ce79e585df9d7b0853de0c1c6df431bb6a1507cf
lavistar <wila@tistory.com>
2019-10-15 12:10:01 +0900

    1015 commit2
    lunch

--- ml08.r
+++ ml08.r
@@ -5,7 +5,7 @@ wine <- read.csv(file = "mlwr/whitewines.csv")
 
 #2 데이터 확인 전처리
 str(wine)
- # 4,898 obs.(예시), 12variables(특징) - white wine 데이터
+# 4,898 obs.(예시), 12variables(특징) - white wine 데이터
 summary(wine) # 기술통계량
 # 종속 변수 (quality)의 분포
 hist(wine$quality)
@@ -54,3 +54,29 @@ MAE <- function (actual, predict){
 MAE(actual = c(1,2,3),predict = c(1.1, 1.9, 3.0))
 
 MAE(wine_test$quality, wine_predict) # 값이 작을수록 정확하다 
+
+#5 모델 성능 향상
+# 모델 트리(Model Tree):
+# Regression Tree(분류) + Regression Modeling(회귀 모델 적용)
+# 교재(R을 활용한 러신 머닝 2/e) : RWeka 패키지의 M5P 함수 사용
+# Cubist 패키지: 규칙 학습 기반 분류 + M5P 알고리즘 회귀 모델 적용
+install.packages("Cubist")
+library(Cubist)
+#cubist(x = 훈련 데이터, y = 훈련 데이터의 결과)
+wine_cubist <- cubist(x = wine_train[-12], y = wine_train$quality) 
+# 종속변수를 제외한 데이터 프레임을 넣어야 함, 12번째 컬럼 제외
+# (데이터프레임, 훈련 label)
+wine_cubist
+summary(wine_cubist)
+
+# 모델 트리의 성능 테스트 !!
+wine_predict2 <- predict(wine_cubist, wine_test)
+summary(wine_predict2)
+summary(wine_test$quality)
+summary(wine_predict)
+
+# 상관 계수
+cor(wine_predict2, wine_test$quality) #0.64
+
+# MAE: 평균 절대 오차
+MAE(wine_predict2, wine_test$quality)
